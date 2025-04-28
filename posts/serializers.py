879b9b50f297ac65f199e5e3e83a89fbb6fa760d
Samantha Atkinson <sammodeste@yahoo.com>
2025-04-28 12:28:48 +0100

    FEAT: Implement Post API with list and create functionality, add URL routing for posts

--- posts/serializers.py
+++ posts/serializers.py
@@ -8,40 +8,42 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source="owner.profile.id")
     profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
 
+    def validate_image(self, value):
+        """
+        Validate the image field to ensure it is not too:
+        - large (2MB)
+        - wide (4096px)
+        - tall (4096px)
+        """
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError("Image size cannot exceed 2MB!")
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                "Image width cannot exceed 4096px!"
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                "Image height cannot exceed 4096px!"
+            )
 
-def valudate_image(self, value):
-    """
-    Validate the image field to ensure it is not too:
-    - large (2MB)
-    - wide (4096px)
-    - tall (4096px)
-    """
-    if value.size > 1024 * 1024 * 2:
-        raise serializers.ValidationError("Image size cannot exceed 2MB!")
-    if value.image.width > 4096:
-        raise serializers.ValidationError("Image width cannot exceed 4096px!")
-    if value.image.height > 4096:
-        raise serializers.ValidationError("Image height cannot exceed 4096px!")
+        return value
 
-    return value
+    def get_is_owner(self, obj):
+        request = self.context["request"]
+        return request.user == obj.owner
 
-
-def get_is_owner(self, obj):
-    request = self.context["request"]
-    return request.user == obj.owner
-
-
-class Meta:
-    model = Post
-    fields = [
-        "id",
-        "owner",
-        "is_owner",
-        "profile_id",
-        "profile_image",
-        "created_at",
-        "updated_at",
-        "title",
-        "content",
-        "image",
-    ]
+    class Meta:
+        model = Post
+        fields = [
+            "id",
+            "owner",
+            "is_owner",
+            "profile_id",
+            "profile_image",
+            "created_at",
+            "updated_at",
+            "title",
+            "content",
+            "image",
+            "image_filter_choices",
+        ]
