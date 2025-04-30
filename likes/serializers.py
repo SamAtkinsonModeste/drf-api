bac81cfc277baf6b81d5b8be61b7d40798ad39f8
Samantha Atkinson <sammodeste@yahoo.com>
2025-04-30 22:16:22 +0100

    feat: add error handling for duplicate likes in LikeSerializer

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Like
 
@@ -13,3 +14,11 @@ class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = ["id", "owner", "post", "created_at"]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError(
+                {"detail": "You have already liked this post."}
+            )
