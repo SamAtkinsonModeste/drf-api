0b3d0e95d859a1772fd8b5b51c5e26422a21fb07
Samantha Atkinson <sammodeste@yahoo.com>
2025-05-06 12:51:03 +0100

    fix: reorder root_route in urlpatterns for improved URL handling

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 from .views import root_route
 
 urlpatterns = [
-    path("", root_route),
     path("admin/", admin.site.urls),
     path("api-auth", include("rest_framework.urls")),
     path("dj-rest-auth", include("dj_rest_auth.urls")),
@@ -31,4 +30,5 @@ urlpatterns = [
     path("", include("comments.urls")),
     path("", include("likes.urls")),
     path("", include("followers.urls")),
+    path("", root_route),
 ]
