e21e1e30b0f07df8ceee7676624e5c95bf1619c8
Samantha Atkinson <sammodeste@yahoo.com>
2025-05-06 12:47:08 +0100

    fix: specify allowed methods for root_route to only accept GET requests

--- drf_api/views.py
+++ drf_api/views.py
@@ -2,6 +2,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-@api_view()
+@api_view(["GET"])
 def root_route(request):
     return Response({"message": "Welcome to my drf API!"})
