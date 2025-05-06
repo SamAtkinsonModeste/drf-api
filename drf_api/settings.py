27f8d0265a41c32d2c65113f552fb647e18c4240
Samantha Atkinson <sammodeste@yahoo.com>
2025-05-06 11:01:09 +0100

    feat: enhance CommentSerializer with created_at and updated_at fields, add root route view and update settings for pagination and rendering

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -36,7 +36,15 @@ REST_FRAMEWORK = {
             else "dj_rest_auth.jwt_auth.JWTCookieAuthentication"
         )
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DATETIME_FORMAT": "%d %b %Y",
 }
+
+if "DEV" not in os.environ:
+    REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
+        "rest_framework.renderers.JSONRenderer",
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
@@ -57,7 +65,7 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
