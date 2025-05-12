1425a74a60573882d5126faf57affe2cf9d3d604
Samantha Atkinson <sammodeste@yahoo.com>
2025-05-12 09:06:31 +0100

    fix: add missing newline for better readability in settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -65,6 +65,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEBUG" in os.environ
+
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
