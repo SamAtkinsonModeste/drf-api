a28ceaa51ae6cee0acd8f8700b3748b99e3c35d1
Samantha Atkinson <sammodeste@yahoo.com>
2025-05-12 08:48:54 +0100

    feat: add cloudinary storage configuration for media and static files

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -204,9 +204,11 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
-# CSRF_COOKIE_SAMESITE = "None"
-# CSRF_COOKIE_SECURE = True
-# SESSION_COOKIE_SAMESITE = "None"
-# SESSION_COOKIE_SECURE = True
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
