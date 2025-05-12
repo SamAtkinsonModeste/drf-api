ef8c9eee4652c9165ffa1a928ad1ccf76a6aad6f
Samantha Atkinson <sammodeste@yahoo.com>
2025-05-12 08:57:18 +0100

    fix: update DEBUG setting to check for environment variable directly

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -64,8 +64,7 @@ REST_AUTH_SERIALIZERS = {
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEV") == "1"
-
+DEBUG = "DEBUG" in os.environ
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
